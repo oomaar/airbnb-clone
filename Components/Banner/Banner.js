2633d4c5b6eed3fb56165aef156226f1ce06e9cd
Omar <omaar_5@hotmail.com>
2021-08-04 15:05:55 +0200

    complete: Banner part-3

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -5,7 +5,7 @@ export const Banner = () => {
             md:justify-start p-10"
         >
             <div className="flex flex-col justify-center items-center text-center md:w-1/3 lg:w-1/2
-                xl:w-1/4 md:text-left lg:ml-40 md:mt-2"
+                xl:w-1/4 md:text-left lg:ml-32 md:mt-2 lg:mt-40"
             >
                 <h1 className="text-4xl font-bold mb-10 md:text-5xl lg:text-7xl">
                     Olympian & Paralympian Online Experiences

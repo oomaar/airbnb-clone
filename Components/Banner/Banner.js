dedcb875e288d6c5b97a59148b13c6133b6fc576
Omar <omaar_5@hotmail.com>
2021-08-04 03:16:51 +0200

    complete: Banner part-2

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -5,12 +5,12 @@ export const Banner = () => {
             md:justify-start p-10"
         >
             <div className="flex flex-col justify-center items-center text-center md:w-1/3 lg:w-1/2
-                xl:w-1/4 md:text-left lg:ml-40"
+                xl:w-1/4 md:text-left lg:ml-40 md:mt-2"
             >
                 <h1 className="text-4xl font-bold mb-10 md:text-5xl lg:text-7xl">
                     Olympian & Paralympian Online Experiences
                 </h1>
-                <button className="bg-white text-black p-2 rounded-md cursor-pointer">
+                <button className="bg-white text-black p-2 rounded-md cursor-pointer md:self-start">
                     Explore Now
                 </button>
             </div>

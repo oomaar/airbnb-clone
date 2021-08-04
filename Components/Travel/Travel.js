1151764ce85415e47b849f2edf56ed5de4b8a7f7
Omar <omaar_5@hotmail.com>
2021-08-04 17:46:17 +0200

    fix: Travel Bug

--- Components/Travel/Travel.js
+++ Components/Travel/Travel.js
@@ -7,12 +7,13 @@ export const Travel = () => {
                     className="w-full rounded-xl"
                 />
             </div>
-            <div className="absolute top-0 sm:top-1/3 left-10 w-1/3">
-                <h3 className="text-lg sm:text-2xl md:text-4xl xl:text-6xl text-black font-bold">
+            <div className="absolute top-5 sm:top-1/3 left-10 w-full sm:w-1/3 flex sm:flex-col items-center">
+                <h3 className="text-lg sm:text-2xl md:text-4xl w-1/4 sm:w-full mr-8 sm:mr-0 xl:text-6xl 
+                    text-black font-bold">
                     Not sure where to go? Perfect.
                 </h3>
                 <button className="bg-black bg-opacity-90 text-white text-sm p-2 rounded-md my-2 cursor-pointer
-                    md:text-md md:my-4 lg:text-lg lg:my-6">
+                    md:text-md md:my-4 lg:text-lg lg:my-6 whitespace-nowrap sm:self-start">
                     I'm Flexable
                 </button>
             </div>
